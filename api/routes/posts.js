71323c96d99a2e8a1984beb56aefee14e8fd5d98
Joshua Warner <joshuawarner51@yahoo.com>
2022-07-20 12:13:26 +0100

    Added create routing to posts.js

---
+++ api/routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const postsController = require('../controllers/posts');
+
+router.get('/', postsController.index);
+router.get('/:id', postsController.show);
+router.post('/', postsController.create)
+
+module.exports = router;
